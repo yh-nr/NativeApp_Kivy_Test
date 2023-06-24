5110805ad6548a29a1fba4c968fa1fbff7295f22
yh-nr <ynaru911@gmail.com>
2023-06-25 03:16:46 +0900

    use pyjunius

--- buildozer.spec
+++ buildozer.spec
@@ -37,7 +37,7 @@ version = 0.1
 
 # (list) Application requirements
 # comma separated e.g. requirements = sqlite3,kivy
-requirements = Python3==3.9.13, kivy==2.1.0, cython, japanize_kivy, opencv-python, plyer, pyjnius
+requirements = Python3, kivy==2.1.0, cython, japanize_kivy, opencv-python, plyer, pyjnius
 
 # (str) Custom source folders for requirements
 # Sets custom source for any requirements with recipes
--- sub.py
+++ sub.py
@@ -17,9 +17,22 @@ def savepic(camera, timestr):
             # Android 9以前の場合、従来のストレージアクセスを利用
             app_storage_path = Environment.getExternalStorageDirectory().getAbsolutePath()
     elif platform == 'win':   
+        import ctypes
         import os
-        user_home = os.environ['USERPROFILE']
-        app_storage_path = os.path.join(user_home, "Pictures")
+
+        # CSIDL_MYPICTURES の値は 39
+        CSIDL_MYPICTURES = 39
+
+        # 最大パス長
+        MAX_PATH = 260
+
+        # SHGetFolderPath 関数を呼び出すためのセットアップ
+        shell32 = ctypes.windll.shell32
+        buf = ctypes.create_unicode_buffer(MAX_PATH)
+        shell32.SHGetFolderPathW(0, CSIDL_MYPICTURES, 0, 0, buf)
+
+        # パスを取得
+        app_storage_path = buf.value
     
-    print(app_storage_path+"IMG_{}.png".format(timestr))
-    camera.export_to_png(app_storage_path+"/IMG_{}.png".format(timestr))
+    print(app_storage_path+"\IMG_{}.png".format(timestr))
+    camera.export_to_png(app_storage_path+"\IMG_{}.png".format(timestr))
