075f078600bc3d98a947b1f2f233c270970b6c96
yh-nr <127526695+yh-nr@users.noreply.github.com>
2023-06-27 17:29:18 +0900

    main.py の更新

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from kivy.app import App            
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
-from kivy.properties import ObjectProperty
+#from kivy.properties import ObjectProperty
 
 from kivy.config import Config
 Config.set('graphics', 'width', '480')
