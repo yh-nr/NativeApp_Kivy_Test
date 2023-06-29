6406f74a1b8d6051eee5a9ee7d181ea826fbba0b
yh-nr <127526695+yh-nr@users.noreply.github.com>
2023-06-29 22:14:26 +0900

    Page2.py の更新

--- src/Page2.py
+++ src/Page2.py
@@ -13,14 +13,4 @@ class CameraClick(BoxLayout):
         timestr = time.strftime("%Y%m%d_%H%M%S")
         filepath = SavePic(self.camera_ref, timestr)
         message = f"Captured ({filepath})"
-        self.show_notification(message=message)
-
-
-    def show_notification(self, message):
-            notification.notify(
-                title='My Notification',
-                message=message,
-                app_icon='',
-                timeout=15,
-                toast=True
-            )
+        app.show_notification(message=message)
